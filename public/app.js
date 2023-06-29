6048ffd47fb3fa3309f63e4049fb8b2d1fc77c8f
NASA Psyche <psychemission@gmail.com>
2023-06-29 11:15:13 -0700

    clean-up

--- public/app.js
+++ public/app.js
@@ -1,22 +1,3 @@
-// function passwordProtecion() {
-// 	let content = document.getElementById("projectCards");
-// 	content.style.display = "none";
-
-// 	let passwordSection = document.getElementById('passwordProtection');
-// 	let incorrectPassword = document.getElementById('incorrectPasswordError');
-// 	incorrectPassword.style.display = "none";
-
-// 	document.getElementById('passwordSubmitBtn').addEventListener('click', () => {
-// 		if (document.getElementById('passwordInput').value == "LBn6Hawq2NTApQ9") {
-// 			content.style.display = "block";
-// 			passwordSection.style.display = "none";
-// 			incorrectPassword.style.display = "none";
-// 		} else {
-// 			incorrectPassword.style.display = "block";
-// 		}
-// 	});
-// }
-
 // Project List for all Web Based Projects (Games and WebXR)
 // src/data/games.js
 const games = [
@@ -111,7 +92,7 @@ const games = [
 	// Add more games as needed
 ];
 
-// Creates the cards for all projects with two buttons
+// Creates the cards for all projects with play button
 for (let index = 0; index < games.length; index++) {
 	let card = document.createElement("div");
 	card.className = "card";
@@ -120,7 +101,6 @@ for (let index = 0; index < games.length; index++) {
 	card.setAttribute("game-age", games[index].age);
 	card.setAttribute("game-difficulty", games[index].difficulty);
 	card.classList.add(
-		// "border",
 		"border-0",
 		"col-3",
 		"p-0",
@@ -135,7 +115,6 @@ for (let index = 0; index < games.length; index++) {
 	let cardTitle = document.createElement("h6");
 	cardTitle.innerHTML = games[index].id.split("-").join(" ").toUpperCase();
 	cardTitle.classList.add("card-title");
-	// cardTitle.style.color = "#908a9c";
 
 	let projectScreenshot = document.createElement("img");
 	projectScreenshot.classList.add("card-img-top");
@@ -148,31 +127,11 @@ for (let index = 0; index < games.length; index++) {
 	projectScreenshot.style.borderTop = "none";
 	projectScreenshot.style.borderBottom = "0.5px solid white";
 
-	let btnGroup = document.createElement("div");
-	btnGroup.className = "btn-group";
-	btnGroup.setAttribute("role", "group");
-
-	// Add a "Report a Bug" button into the each game chicklet.
-
-	// let bugBtn = document.createElement("button");
-	// bugBtn.setAttribute("type", "button");
-	// bugBtn.setAttribute("data-bs-toggle", "modal");
-	// bugBtn.setAttribute("data-bs-target", "#bugReportModal");
-	// bugBtn.setAttribute("title", "Report a bug");
-	// bugBtn.classList.add("btn","btn-sm", "btn-danger", "rounded", "ms-5", "hidden");
-	// bugBtn.onclick = handleBugBtn.bind(this, games[index].id);
-
-	// let bugText = document.createElement("span");
-	// bugText.classList.add("bug-text");
-	// bugText.innerHTML = "Report a Bug";
-
-	// bugBtn.appendChild(bugText);
-
-
 	// Add a "Play" button into the each game chicklet.
 	let playLink = document.createElement("div");
 	playLink.className = "play-link";
 	playLink.style.cursor = "pointer";
+	playLink.style.width = "2.8rem";
 	var game_window = "";
 	playLink.onclick = () => {
 		if (games[index].id == "journey-to-psyche") {
@@ -187,7 +146,6 @@ for (let index = 0; index < games.length; index++) {
         });
 	};
 	let playText = document.createElement("p");
-	// playText.style.fontSize = "15px";
 	playText.style.letterSpacing = "1px";
 	playText.style.marginTop = "7px";
 	playText.innerHTML = "Play";
@@ -199,30 +157,78 @@ for (let index = 0; index < games.length; index++) {
 	playDivider1.style.height = "2px";
 	playDivider1.style.float = "left";
 	playDivider1.style.background = "linear-gradient(to right, rgb(29, 13, 68), rgb(89, 38, 81) 30%, rgb(165, 63, 91) 50%, rgb(239, 89, 102) 70%, rgb(244, 124, 51) 90%, rgb(249, 160, 0))";
-
-	// let playDivider2 = document.createElement("div");
-	// playDivider2.className = "divider2"
-	// playDivider2.style.marginTop = "-15px";
-	// playDivider2.style.width = "100%";
-	// playDivider2.style.height = "2px";
-	// playDivider2.style.background = "linear-gradient(to right, rgb(29, 13, 68), rgb(89, 38, 81) 30%, rgb(165, 63, 91) 50%, rgb(239, 89, 102) 70%, rgb(244, 124, 51) 90%, rgb(249, 160, 0))";
-
 	playLink.appendChild(playText);
 	playLink.appendChild(playDivider1);
-	// playLink.appendChild(playDivider2);
 
-
-
-	btnGroup.appendChild(playLink);
-	// btnGroup.appendChild(bugBtn);
+	// btnGroup.appendChild(playLink);
 
 	cardContent.appendChild(cardTitle);
-	cardContent.appendChild(btnGroup);
+	cardContent.appendChild(playLink);
 
 	card.appendChild(projectScreenshot);
 	card.appendChild(cardContent);
 	document.getElementById("projectsGroup16").appendChild(card);
 }
+
+// Add event listeners to the filter checkboxes
+const filterCheckboxes = document.querySelectorAll('.form-check-input');
+filterCheckboxes.forEach(checkbox => {
+	if (checkbox.id == "themeToggle") {
+
+	}
+	else {
+		checkbox.addEventListener('change', applyFilters);
+	}
+
+});
+
+function applyFilters() {
+	const cards = document.querySelectorAll('#projectsGroup16 .card');
+	const anyFilterChecked = document.querySelector('#filterAccordion input[type=checkbox]:checked') !== null;
+	cards.forEach(card => {
+		const cardClass = card.getAttribute('game-class');
+		const cardGenre = card.getAttribute('game-genre');
+
+		const cardAge = card.getAttribute('game-age');
+		const cardDifficulty = card.getAttribute('game-difficulty');
+
+		const classFilter = document.querySelector(`input[type=checkbox][id$=Class][value="${cardClass}"]:checked`);
+		const genreFilter = document.querySelector(`input[type=checkbox][id$=Genre][value="${cardGenre}"]:checked`);
+		const ageFilter = document.querySelector(`input[type=checkbox][id$=Age][value="${cardAge}"]:checked`);
+		const difficultyFilter = document.querySelector(`input[type=checkbox][id$=Difficulty][value="${cardDifficulty}"]:checked`);
+
+
+
+		if (classFilter || genreFilter || ageFilter || difficultyFilter || !anyFilterChecked) {
+			card.style.display = card.getAttribute('data-original-display');
+		} else {
+			card.style.display = 'none';
+		}
+	});
+}
+
+
+// ============================================================================================================================================================
+
+// function passwordProtecion() {
+// 	let content = document.getElementById("projectCards");
+// 	content.style.display = "none";
+
+// 	let passwordSection = document.getElementById('passwordProtection');
+// 	let incorrectPassword = document.getElementById('incorrectPasswordError');
+// 	incorrectPassword.style.display = "none";
+
+// 	document.getElementById('passwordSubmitBtn').addEventListener('click', () => {
+// 		if (document.getElementById('passwordInput').value == "LBn6Hawq2NTApQ9") {
+// 			content.style.display = "block";
+// 			passwordSection.style.display = "none";
+// 			incorrectPassword.style.display = "none";
+// 		} else {
+// 			incorrectPassword.style.display = "block";
+// 		}
+// 	});
+// }
+
 
 // Resets the form when opened and also handles the submit button
 // function handleBugBtn(projectID) {
@@ -270,40 +276,3 @@ for (let index = 0; index < games.length; index++) {
 // 			})
 // 	})
 // }
-
-// Add event listeners to the filter checkboxes
-const filterCheckboxes = document.querySelectorAll('.form-check-input');
-filterCheckboxes.forEach(checkbox => {
-	if (checkbox.id == "themeToggle") {
-
-	}
-	else {
-		checkbox.addEventListener('change', applyFilters);
-	}
-
-});
-
-function applyFilters() {
-	const cards = document.querySelectorAll('#projectsGroup16 .card');
-	const anyFilterChecked = document.querySelector('#filterAccordion input[type=checkbox]:checked') !== null;
-	cards.forEach(card => {
-		const cardClass = card.getAttribute('game-class');
-		const cardGenre = card.getAttribute('game-genre');
-
-		const cardAge = card.getAttribute('game-age');
-		const cardDifficulty = card.getAttribute('game-difficulty');
-
-		const classFilter = document.querySelector(`input[type=checkbox][id$=Class][value="${cardClass}"]:checked`);
-		const genreFilter = document.querySelector(`input[type=checkbox][id$=Genre][value="${cardGenre}"]:checked`);
-		const ageFilter = document.querySelector(`input[type=checkbox][id$=Age][value="${cardAge}"]:checked`);
-		const difficultyFilter = document.querySelector(`input[type=checkbox][id$=Difficulty][value="${cardDifficulty}"]:checked`);
-
-
-
-		if (classFilter || genreFilter || ageFilter || difficultyFilter || !anyFilterChecked) {
-			card.style.display = card.getAttribute('data-original-display');
-		} else {
-			card.style.display = 'none';
-		}
-	});
-}
